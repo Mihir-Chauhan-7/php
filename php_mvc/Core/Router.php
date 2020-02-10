80f400d91197f4bdbeb8a08b6fc3b7659bef384d
Mihir Chauhan <chauhanmihir51@gmail.com>
2020-02-10 09:36:44 +0530

    Practice 7th Feb

--- php_mvc/Core/Router.php
+++ php_mvc/Core/Router.php
@@ -4,18 +4,32 @@ class Router{
     protected $routes=[];
     protected $path=[];
 
-    public function addRoute($route,$path)
+    public function addRoute($route,$path=[])
     {
+        $route = preg_replace('/\//', '\\/', $route);
+
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+        
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route]=$path;
     }
 	public function matchRoute($url)
 	{
 		foreach ($this->routes as $route => $path) {
-			if($url == $route)
-			{
-				$this->path=$path;
-				return true;
-			}
+            if(preg_match($route,$url,$matches))
+            {
+                foreach($matches as $key => $match){
+                    if(is_string($key))
+                    {
+                        $path[$key]=$match;
+                    }
+                }
+                $this->path=$path;
+                return true;
+            }
 		}
 		return false;
 	}
--- php_mvc/public/index.php
+++ php_mvc/public/index.php
@@ -8,12 +8,18 @@ echo "URL : ".$_SERVER['QUERY_STRING'];
 $router->addRoute('',['controller' => 'Home','action' => 'index']);
 $router->addRoute('posts',['controller' => 'Posts','action' => 'index']);
 $router->addRoute('posts/new',['controller' => 'Posts','action' => 'new']);
+$router->addRoute('{controller}/{action}');
+//$router->addRoute('{controller}/{id:\d+}/{action}');
+$router->addRoute('admin{action}/{controller}');
+
+
 
 if($router->matchRoute($_SERVER['QUERY_STRING']))
 {
 	echo "<pre>";
-    print_r($router->getPath());
-	echo "</pre>";	
+	print_r($router->getRoutes());
+    echo htmlspecialchars(print_r($router->getPath(),true));
+	echo "</pre>";
 }
 else
 {
